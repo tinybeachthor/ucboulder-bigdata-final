c58e0afacb9959976a41dff01eb9e03e642c031a
Martin Toman <toman.martin@live.com>
2024-03-22 17:19:50 -0700

    Collect: push new articles to queue

--- apps/collect.py
+++ apps/collect.py
@@ -1,22 +1,41 @@
 import arxiv
+import os
+import pika
 
-def main():
-    # Construct the default API client.
-    client = arxiv.Client()
+def pull_arxiv(client, channel, queue='arxiv'):
 
-    # Search for the 10 most recent articles
+    # Search for recent articles
     search = arxiv.Search(
         query="cat:cs.AI",
-        max_results = 10,
+        max_results = 5,
         sort_by = arxiv.SortCriterion.SubmittedDate,
     )
     print(search)
 
+    # Handle results
     results = client.results(search)
-
     for r in client.results(search):
         print(r.title)
+        body = r.title
+        channel.basic_publish(exchange='', routing_key=queue, body=body)
 
 
 if __name__ == '__main__':
-    main()
+
+    # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
+    queue_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
+
+    # Setup
+    client = arxiv.Client()
+
+    params = pika.URLParameters(queue_url)
+    connection = pika.BlockingConnection(params)
+    channel = connection.channel()
+
+    channel.queue_declare(queue='arxiv') # Declare a queue
+
+    # Execute
+    pull_arxiv(client, channel)
+
+    # Cleanup
+    connection.close()
