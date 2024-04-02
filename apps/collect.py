1b629bc09aa2e5b3c9d827bac4a7cffd59c28d56
Martin Toman <toman.martin@live.com>
2024-04-02 12:29:43 -0700

    Collect: logging

--- apps/collect.py
+++ apps/collect.py
@@ -3,10 +3,14 @@ import os
 import pika
 from datetime import datetime
 from dateutil import tz
+import logging
 
 from models.arxiv import Article as ArxivArticle
 
+log = logging.getLogger(__name__)
+
 def pull_arxiv(client, channel, queue):
+    log.info('pulling arxiv')
 
     # Search for recent articles
     search = arxiv.Search(
@@ -18,13 +22,22 @@ def pull_arxiv(client, channel, queue):
     # Handle results
     results = client.results(search)
     for r in client.results(search):
+        try:
+            log.info(f'got article {r.entry_id}')
+            published = r.published.astimezone(tz.UTC)
+            authors = list(map(str, r.authors))
+            article = ArxivArticle(r.entry_id, published, r.title, authors, r.summary)
+        except Exception as e:
+            log.warn(e)
+            continue
 
-        published = r.published.astimezone(tz.UTC)
-        authors = list(map(str, r.authors))
-        article = ArxivArticle(r.entry_id, published, r.title, authors, r.summary)
-
-        body = article.toJSON()
-        channel.basic_publish(exchange='', routing_key=queue, body=body)
+        try:
+            body = article.toJSON()
+            channel.basic_publish(exchange='', routing_key=queue, body=body)
+            log.info(f'queued article {r.entry_id}')
+        except Exception as e:
+            log.warn(e)
+            continue
 
 
 def collect():
@@ -33,6 +46,7 @@ def collect():
     queue_url = os.environ.get('CLOUDAMQP_URL', 'amqp://guest:guest@localhost:5672/%2f')
 
     # Setup
+    log.info('setup start')
     client = arxiv.Client()
 
     params = pika.URLParameters(queue_url)
@@ -42,8 +56,14 @@ def collect():
     # Declare queues
     channel.queue_declare(queue=ARXIV_QUEUE_NAME)
 
+    log.info('setup done')
+
     # Execute
+    log.info('execute start')
+
     pull_arxiv(client, channel, queue=ARXIV_QUEUE_NAME)
+
+    log.info('execute done')
 
     # Cleanup
     connection.close()
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import argparse
 import sys
+import logging
 
 from apps.collect import collect
 from apps.process import process
 from apps.web import app
 
+log = logging.getLogger(__name__)
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    log.info('started')
+
     parser = argparse.ArgumentParser()
     parser.add_argument("command")
 
@@ -18,3 +24,5 @@ if __name__ == '__main__':
     else:
         print(f"Unknown command: {args.command}")
         sys.exit(1)
+
+    log.info('finished')
