40bbcf1b61b39561cc56dd3e46724b81e0804faa
Martin Toman <toman.martin@live.com>
2024-04-05 14:50:44 -0700

    Process: add logging

--- apps/process.py
+++ apps/process.py
@@ -2,19 +2,28 @@ import os
 import functools
 import pika
 import psycopg
+import logging
 
 from models.arxiv import Article as ArxivArticle
 from components.database import insert_arxiv
 
+log = logging.getLogger(__name__)
+
 def process_arxiv(ch, method, properties, body, args):
-    article = ArxivArticle.fromJSON(body.decode())
-    print(article.id)
+    try:
+        article = ArxivArticle.fromJSON(body.decode())
+        log.info('process_arxiv', article.id)
+    except Exception as e:
+        log.warn(e)
+        ch.basic_ack(delivery_tag = method.delivery_tag)
+        return
 
     conn = args
     with conn.cursor() as cur:
         insert_arxiv(cur, article)
         conn.commit()
 
+    log.info('process_arxiv done', article.id)
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 
@@ -27,6 +36,8 @@ def process():
         'DATABASE_URL', 'postgresql://guest:guest@localhost:5432/content')
 
     # Setup
+    log.info('setup start')
+
     params = pika.URLParameters(queue_url)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
@@ -34,7 +45,11 @@ def process():
     # Declare a queue
     channel.queue_declare(queue=ARXIV_QUEUE_NAME)
 
+    log.info('setup done')
+
     # Setup database connection
+    log.info('execute start')
+
     with psycopg.connect(database_url) as conn:
 
         # Execute
@@ -42,6 +57,8 @@ def process():
         channel.basic_consume(ARXIV_QUEUE_NAME, callback, auto_ack=False)
 
         channel.start_consuming()
+
+    log.info('execute done')
 
     # Cleanup
     connection.close()
